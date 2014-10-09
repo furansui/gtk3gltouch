f3720cdfb0e0a2e601b57516233ab722b5091451
furansui <furansui@gmail.com>
2014-10-09 23:06:01 +0900

    add some cout

--- geissTry.cpp
+++ geissTry.cpp
@@ -1,5 +1,8 @@
 //g++ geissTry.cpp -o geissTry.xps0 -lgeis -fpermissive -lX11
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
 #include <X11/Xlib.h>
 #include <geis/geis.h>
 #include <stdio.h>
@@ -17,6 +20,70 @@ static Window getRootWindow() {
   display = XOpenDisplay(NULL);
   XGetInputFocus(display, &focus, &revert);
   return focus;
+}
+
+std::string gestures[] = {"Drag,touch=2","Pinch,touch=2"};
+
+struct GestureLog {
+  long int timestamp;
+  double focusx[2];
+  int touches;
+  std::pair<int,int> boundingbox[2];
+  int position[2];
+  std::vector<int> touchid;
+  std::vector<std::pair<double,double> > touchcoord;
+} gestureLog;
+
+static void gesture_start(GeisGestureType gesture_type,
+			  GeisGestureId gesture_id,
+			  GeisSize attr_count, GeisGestureAttr * attrs, bool endGesture) 
+{
+  gestureLog.touchid.clear();
+  gestureLog.touchcoord.clear();
+
+}
+
+static void gesture_match(GeisGestureType gesture_type,
+			  GeisGestureId gesture_id,
+			  GeisSize attr_count, GeisGestureAttr * attrs, bool endGesture) 
+{
+  for(int i=0;i<2;i++) {
+    if(strcmp(gestures[i].c_str(),attrs[7].string_val) == 0) {
+      //cout << "this is " << gestures[i] << endl;
+    }
+  }
+
+  gestureLog.boundingbox[0].first = attrs[9].float_val;
+  gestureLog.boundingbox[0].second = attrs[10].float_val;
+  gestureLog.boundingbox[1].first = attrs[11].float_val;
+  gestureLog.boundingbox[1].second = attrs[12].float_val;
+  
+  cout << std::setw(10);
+  cout << gestureLog.boundingbox[0].first << " ";
+  cout << gestureLog.boundingbox[0].second << " ";
+  cout << gestureLog.boundingbox[1].first << " ";
+  cout << gestureLog.boundingbox[1].second << " ";
+  cout << endl;
+}
+
+static void printAttr(GeisGestureAttr * attr) {
+  cout << "attr " << attr->name << " = ";
+  switch (attr->type) {
+  case GEIS_ATTR_TYPE_BOOLEAN:
+    cout << attr->boolean_val;break;
+  case GEIS_ATTR_TYPE_FLOAT:
+    cout <<  attr->float_val;
+    break;
+  case GEIS_ATTR_TYPE_INTEGER:
+    cout << attr->integer_val;
+    break;
+  case GEIS_ATTR_TYPE_STRING:
+    cout << attr->string_val;
+    break;
+  default:
+    break;
+  }
+  cout << endl;
 }
 
 static void gesture_added(void *cookie,
@@ -44,6 +111,11 @@ static void gesture_start(void *cookie,
 			  GeisGestureAttr * attrs)
 {
   cout << "gesture start" << endl;
+  gesture_start(gesture_type, gesture_id, attr_count, attrs, false);
+  for(int i=0;i<attr_count;i++) {
+    cout << std::dec << i << " ";
+    printAttr(&attrs[i]);
+  }
 }
 
 static void gesture_update(void *cookie,
@@ -52,7 +124,7 @@ static void gesture_update(void *cookie,
 			  GeisSize attr_count, 
 			  GeisGestureAttr * attrs)
 {
-  cout << "gesture update " << attrs[7].string_val << endl;
+  gesture_match(gesture_type, gesture_id, attr_count, attrs, false);
 }
 
 static void gesture_finish(void *cookie,
@@ -62,6 +134,7 @@ static void gesture_finish(void *cookie,
 			  GeisGestureAttr * attrs)
 {
   cout << "gesture finish" << endl;
+  gesture_match(gesture_type, gesture_id, attr_count, attrs, true);
 }
 
 GeisGestureFuncs gesture_funcs = {
@@ -109,10 +182,10 @@ int main(int argc, char**argv) {
   }
 
   char* gestures_list[20] = {NULL};
-  gestures_list[0] = (char*)malloc(sizeof(char[16]));
-  sprintf(gestures_list[0],"Drag,touch=2");
-  gestures_list[1] = (char*)malloc(sizeof(char[16]));
-  sprintf(gestures_list[1],"Pinch,touch=2");
+  for(int i=0;i<2;i++) {
+    gestures_list[i] = (char*)malloc(sizeof(char[16]));
+    sprintf(gestures_list[i],"%s",gestures[i].c_str());
+  }
 
   status = geis_subscribe(instance, GEIS_ALL_INPUT_DEVICES, (const char**)gestures_list, &gesture_funcs, NULL);
   if (status != GEIS_STATUS_SUCCESS) {
